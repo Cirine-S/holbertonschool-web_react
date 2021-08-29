e7c7489734fcfeb7e7f5c9925657c9dbb87b8d14
cirine <1410@holbertonschool.com>
2021-08-29 14:16:08 +0100

    app update

--- 0x04-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x04-React_component/task_1/dashboard/src/App/App.test.js
@@ -1,10 +1,60 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App'
+import { expect } from 'chai';
 
-describe('<App />', () => {
-  it('renders an <App /> component', () => {
+import App from './App';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+
+describe('App', () => {
+    test('renders without crashing', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.exists());
+});
+  test('renders Notifications component', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper).toHaveLength(1);
-	});
+    const notifs = wrapper.find(Notifications);
+
+    expect(notifs).to.have.lengthOf(1);
+  });
+
+  test('renders Header component', () => {
+    const wrapper = shallow(<App />);
+    const header = wrapper.find(Header);
+
+    expect(header).to.have.lengthOf(1);
+  });
+
+  test('renders Login component', () => {
+    const wrapper = shallow(<App />);
+    const login = wrapper.find(Login);
+
+    expect(login).to.have.lengthOf(1);
+  });
+
+  test('renders Footer component', () => {
+    const wrapper = shallow(<App />);
+    const footer = wrapper.find(Footer);
+
+    expect(footer).to.have.lengthOf(1);
+  });
+  test('course list NOT displayed by default', () => {
+    const wrapper = shallow(<App />);
+    const courseList = wrapper.find(CourseList);
+
+    expect(courseList).to.have.lengthOf(0);
+  });
+
+  test('if logged in, course list is displayed and login form is NOT', () => {
+    const wrapper = shallow(<App isLoggedIn={true} />);
+
+    const login = wrapper.find(Login);
+    const courseList = wrapper.find(CourseList);
+
+    expect(login).to.have.lengthOf(0);
+    expect(courseList).to.have.lengthOf(1);
+  });
 });
