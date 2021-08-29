8b7a1cb311e95e2ef50f54213121d9e5da5069ea
cirine <1410@holbertonschool.com>
2021-08-29 15:18:49 +0100

    pure component

---
+++ 0x04-React_component/task_5/dashboard/src/Login/Login.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import Login from './Login';
+
+describe('Login', () => {
+  test('renders without crashing', () => {
+    const wrapper = shallow(<Login />);
+
+    expect(wrapper.exists());
+  });
+
+  test('renders two input tags', () => {
+    const wrapper = shallow(<Login />);
+
+    const inputs = wrapper.find('input');
+    const labels = wrapper.find('label');
+
+    expect(inputs).to.have.lengthOf(2);
+    expect(labels).to.have.lengthOf(2);;
+  });
+});
